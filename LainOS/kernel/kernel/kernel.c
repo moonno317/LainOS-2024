4b3d76ed4a5de0f37b176f95766182d0226f9d1a
moonno317 <139625145+moonno317@users.noreply.github.com>
2024-04-09 19:13:54 +0700

    Update kernel.c

--- LainOS/kernel/kernel/kernel.c
+++ LainOS/kernel/kernel/kernel.c
@@ -9,35 +9,30 @@
 #include <stdatomic.h>
 #include "schedule.h"
 
-#define PAGE_SIZE 4096  // Updated for most x86 systems
+#define PAGE_SIZE 4096  
 #define PAGE_DIRECTORY_ENTRIES 1024
 
 uint32_t page_directory[PAGE_DIRECTORY_ENTRIES] __attribute__((aligned(4096)));
 uint32_t first_page_table[PAGE_DIRECTORY_ENTRIES] __attribute__((aligned(4096)));
 
 void load_page_directory(unsigned int *page_directory_addr) {
-  asm volatile ("mov %0, %%cr3" : : "r" (page_directory_addr) : "memory");
-}
+  asm volatile ("mov %0, %%cr3" : : "r" (page_directory_addr) : "memory"); }
 
 void enable_paging(void) {
   asm volatile ("mov %%cr0, %%eax\n"
                 "orl $0x80000000, %%eax\n"
-                "mov %%eax, %%cr0" : : : "eax", "memory");
-}
+                "mov %%eax, %%cr0" : : : "eax", "memory"); }
 
 void paging() {
     for (int i = 0; i < PAGE_DIRECTORY_ENTRIES; i++) {
-        page_directory[i] = 0x00000002;  // Set all entries to "not present"
-    }
+        page_directory[i] = 0x00000002; }
 
     for (int i = 0; i < PAGE_SIZE; i++) {
-        first_page_table[i] = (i * PAGE_SIZE) | 3;  // Map first 4 MiB of memory
-    }
+        first_page_table[i] = (i * PAGE_SIZE) | 3;  }
 
-    page_directory[0] = ((unsigned int)first_page_table) | 3;  // Point first entry to first page table
+    page_directory[0] = ((unsigned int)first_page_table) | 3; 
     load_page_directory(page_directory);
-    enable_paging();
-}
+    enable_paging(); }
 
 void kernel_main(void) {
     init_gdt();
@@ -46,7 +41,6 @@ void kernel_main(void) {
     paging();
     while (1) {
         schedule();
-        kbd();
-    }
+        kbd(); }
 }
 
